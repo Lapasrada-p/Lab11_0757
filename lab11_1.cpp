3bd73c95f612b7e955b16a79c69c2620c632e3c7
Lapasrada-p <76034405+Lapasrada-p@users.noreply.github.com>
2021-01-15 15:50:59 +0700

    completed

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,16 +1,21 @@
 #include<iostream>
 using namespace std;
 
-int fibonacci(int);
+long long int fibonacci(long long int);
 
-int main(){
+/*int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}*/
+
+int main(){
+    cout << fibonacci(50);
+    return 0; 
 }
 
-int fibonacci(int x){
+long long int fibonacci(long long int x){
     if(x==0) return 0;
     else if (x==1) return 1;
     return fibonacci(x-1)+fibonacci(x-2);  
